ebc72591ea0a7e2156b5d60f8082dca57c47a69a
luwis93choi <luwis93choi@hotmail.com>
2021-02-25 16:01:34 +0900

    Editted extrinsic reward for the agnet training

--- maze_env.py
+++ maze_env.py
@@ -8,250 +8,255 @@ from maze_drawer import Maze_drawer
 from maze_generator import Maze_generator
 
 import time
-
+import cv2 as cv
 import math
 
 class Maze():
 
-    def __init__(self, height, width, detection_range=2):
+    def __init__(self, height, width, detection_range=2, obstacle_occupancy_prob=0.3):
 
         # Init Maze with random 0 and 1 distribution
         self.height = height
         self.width = width
 
-        # Init Agent's starting point as the center of maze
-        # self.curr_agent_pose = [int(self.height/2), int(self.width/2)]
-        # self.prev_agent_pose = [int(self.height/2), int(self.width/2)]
+        # Init Agent's starting point
         self.curr_agent_pose = [0, 0]
-        self.prev_agent_pose = [0, 0]
+        self.prev_point_val = 0
 
-        self.obstacle_occupancy_prob = 0.5
-        self.maze_generator = Maze_generator(height=self.height, width=self.width, obstacle_occupancy_prob=self.obstacle_occupancy_prob)      # Generate valid maze using A star
+        # Init Target point
+        self.target_pose = [self.height-1, self.width-1]
 
-        self.maze, self.target_pose, self.optimal_path = self.maze_generator.generate()
+        # Build random maze based on occupancy probability
+        self.obstacle_occupancy_prob = obstacle_occupancy_prob
+        self.maze_generator = Maze_generator(height=self.height, width=self.width, agent_pose=self.curr_agent_pose, target_pose=self.target_pose, obstacle_occupancy_prob=self.obstacle_occupancy_prob)      # Generate valid maze using A star
 
-        self.maze[self.curr_agent_pose[0]][self.curr_agent_pose[1]] = -1    # Place Agent in the maze
-
-        self.maze[self.target_pose[0]][self.target_pose[1]] = 2         # Place Target location in the maze
+        self.maze, self.curr_agent_pose, self.target_pose, self.optimal_path = self.maze_generator.generate()
 
         # Draw the maze
         self.maze_drawer = Maze_drawer(self.maze)
 
-        # print(self.maze)
+        # Init empty local map
+        self.local_map = 0.5 * np.ones([self.maze.shape[0], self.maze.shape[1]])
+        self.local_map[self.curr_agent_pose[0], self.curr_agent_pose[1]] = 1
+        self.local_map[self.target_pose[0], self.target_pose[1]] = 2
 
-        self.reward = 0
+        # Build initial local map
+        self.detection_range = detection_range
+        for i in range(-1 * (self.detection_range), self.detection_range + 1):
+            for j in range(-1 * (self.detection_range), self.detection_range + 1):
+
+                if (i == 0) and (j == 0): 
+                
+                    self.local_map[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] = 1
+
+                elif self.within_bounds(self.curr_agent_pose[0] + i, 0, self.maze.shape[0]) and self.within_bounds(self.curr_agent_pose[1] + j, 0, self.maze.shape[1]):
+                
+                    if self.maze[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] == 3:
+                
+                        self.local_map[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] = 3
+
+        self.reward = 0.0
         self.done = 0
+        self.collision_count = 0
 
-        states = []
-
-        self.dy = self.target_pose[0] - self.curr_agent_pose[0]
-        self.dx = self.target_pose[1] - self.curr_agent_pose[1]
-
-        self.prev_dy = self.dy
-        self.prev_dx = self.dx
+        self.dy = abs(self.target_pose[0] - self.curr_agent_pose[0])
+        self.dx = abs(self.target_pose[1] - self.curr_agent_pose[1])
 
         self.shortest_dy = self.dy
         self.shortest_dx = self.dx
 
-        states = [self.dy, self.dx, self.curr_agent_pose[0], self.curr_agent_pose[1]]
+        states = self.local_map.flatten()
 
-        self.visited = np.zeros([self.height, self.width])
-        
-        self.detection_range = detection_range
-        for i in range(-1 * (self.detection_range), self.detection_range + 1):
-            for j in range(-1 * (self.detection_range), self.detection_range + 1):
-                if (i == 0) and (j == 0): 
-                    self.visited[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] += -1
-                elif (0 <= self.curr_agent_pose[0] + i) and (self.curr_agent_pose[0] + i < self.height) and (0 <= self.curr_agent_pose[1] + j) and (self.curr_agent_pose[1] + j < self.width):
-                    # states.append(self.maze[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j])
-
-                    if self.maze[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] == 1:
-                        self.visited[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] = 1
-                    else:
-                        self.visited[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] += -1
-                # else:
-                #     states.append(1)
-                    
-        states = np.concatenate((states, self.visited.flatten()))
-        # print(states)
-
-        self.maze_drawer.update_maze(self.visited, curr_pose=self.curr_agent_pose)
+        self.maze_drawer.update_maze(self.local_map, curr_pose=self.curr_agent_pose)
 
     def reset(self):
 
-        # Init Agent's starting point as the center of maze
-        # self.curr_agent_pose = [int(self.height/2), int(self.width/2)]
-        # self.prev_agent_pose = [int(self.height/2), int(self.width/2)]
+        # Init Agent's starting point
         self.curr_agent_pose = [0, 0]
-        self.prev_agent_pose = [0, 0]
+        self.prev_point_val = 0
 
-        self.maze_generator = Maze_generator(height=self.height, width=self.width, obstacle_occupancy_prob=self.obstacle_occupancy_prob)      # Generate valid maze using A star
+        # Init Target point
+        self.target_pose = [self.height-1, self.width-1]
 
-        self.maze, self.target_pose, self.optimal_path = self.maze_generator.generate()
+        # Build random maze based on occupancy probability
+        self.maze_generator = Maze_generator(height=self.height, width=self.width, agent_pose=self.curr_agent_pose, target_pose=self.target_pose, obstacle_occupancy_prob=self.obstacle_occupancy_prob)      # Generate valid maze using A star
 
-        self.maze[self.curr_agent_pose[0]][self.curr_agent_pose[1]] = -1    # Place Agent in the maze
+        self.maze, self.curr_agent_pose, self.target_pose, self.optimal_path = self.maze_generator.generate()
 
-        self.maze[self.target_pose[0]][self.target_pose[1]] = 2         # Place Target location in the maze
+        # Init empty local map
+        self.local_map = 0.5 * np.ones([self.maze.shape[0], self.maze.shape[1]])
+        self.local_map[self.curr_agent_pose[0], self.curr_agent_pose[1]] = 1
+        self.local_map[self.target_pose[0], self.target_pose[1]] = 2
 
-        # print(self.maze)
-
-        self.reward = 0
-        self.done = 0
-
-        states = []
-
-        self.dy = self.target_pose[0] - self.curr_agent_pose[0]
-        self.dx = self.target_pose[1] - self.curr_agent_pose[1]
-
-        self.prev_dy = self.dy
-        self.prev_dx = self.dx
-
-        states = [self.dy, self.dx, self.curr_agent_pose[0], self.curr_agent_pose[1]]
-        
-        self.visited = np.zeros([self.height, self.width])
-        
+        # Build initial local map
         for i in range(-1 * (self.detection_range), self.detection_range + 1):
             for j in range(-1 * (self.detection_range), self.detection_range + 1):
-                if (i == 0) and (j == 0):
-                    self.visited[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] += -1
-                elif (0 <= self.curr_agent_pose[0] + i) and (self.curr_agent_pose[0] + i < self.height) and (0 <= self.curr_agent_pose[1] + j) and (self.curr_agent_pose[1] + j < self.width):
-                    if self.maze[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] == 1:
-                        self.visited[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] = 1
-                    else:
-                        self.visited[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] += -1
-                
-        states = np.concatenate((states, self.visited.flatten()))
 
-        self.maze_drawer.update_maze(self.visited, curr_pose=self.curr_agent_pose)
-        # print(states)
+                if (i == 0) and (j == 0): 
+                
+                    self.local_map[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] = 1
+
+                elif self.within_bounds(self.curr_agent_pose[0] + i, 0, self.maze.shape[0]) and self.within_bounds(self.curr_agent_pose[1] + j, 0, self.maze.shape[1]):
+                
+                    if self.maze[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] == 3:
+                
+                        self.local_map[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] = 3
+                        
+        self.reward = 0.0
+        self.done = 0
+        self.collision_count = 0
+
+        states = self.local_map.flatten()
+
+        self.maze_drawer.update_maze(self.local_map, curr_pose=self.curr_agent_pose)
 
         return states
+
+    def within_bounds(self, value, low, high):
+
+        return (low <= value) and (value <= high)
 
     def maze_update(self, dheight=0, dwidth=0):
         
         skip = False
 
-        if ((self.curr_agent_pose[0] + dheight) < 0) or ((self.curr_agent_pose[0] + dheight) >= self.height):
+        if self.within_bounds(self.curr_agent_pose[0] + dheight, 0, self.maze.shape[0]) is False:
 
-            self.reward += -1000
+            # self.reward += math.sqrt((self.height - self.dy)**2 + (self.width - self.dx)**2)
+            
+            self.reward += -10
             # self.done = 1
-
+            self.collision_count += 1
             skip = True
+            self.maze_drawer.update_maze(self.local_map, curr_pose=self.curr_agent_pose)
 
-        elif ((self.curr_agent_pose[1]) + dwidth < 0) or ((self.curr_agent_pose[1] + dwidth) >= self.width):
-            
-            self.reward += -1000
+        elif self.within_bounds(self.curr_agent_pose[1] + dwidth, 0, self.maze.shape[1]) is False:
+
+            # self.reward += math.sqrt((self.height - self.dy)**2 + (self.width - self.dx)**2)
+
+            self.reward += -10
             # self.done = 1
-            
+            self.collision_count += 1
             skip = True
+            self.maze_drawer.update_maze(self.local_map, curr_pose=self.curr_agent_pose)
 
-        elif self.maze[self.curr_agent_pose[0] + dheight][self.curr_agent_pose[1] + dwidth] == 1:
-            
-            self.reward += -1000
+        elif self.maze[self.curr_agent_pose[0] + dheight][self.curr_agent_pose[1] + dwidth] >= 3:
+
+            # self.reward += math.sqrt((self.height - self.dy)**2 + (self.width - self.dx)**2)
+            # self.local_map[self.curr_agent_pose[0] + dheight][self.curr_agent_pose[1] + dwidth] += 1
+            # self.reward += (-10 - 0.001 * self.local_map[self.curr_agent_pose[0] + dheight][self.curr_agent_pose[1] + dwidth])
+            self.reward += -10
             # self.done = 1
-            
+            self.collision_count += 1
             skip = True
+            self.maze_drawer.update_maze(self.local_map, curr_pose=self.curr_agent_pose)
+
+        # if self.collision_count >= 20:
+        #     self.done = 1
+        #     skip = True
 
         if skip: return
 
-        self.prev_dy = copy.deepcopy(self.dy)
-        self.prev_dx = copy.deepcopy(self.dx)
-
-        self.prev_agent_pose = copy.deepcopy(self.curr_agent_pose)
+        prev_agent_pose = copy.deepcopy(self.curr_agent_pose)
 
         self.curr_agent_pose[0] += dheight
         self.curr_agent_pose[1] += dwidth
 
-        self.dy = self.target_pose[0] - self.curr_agent_pose[0]
-        self.dx = self.target_pose[1] - self.curr_agent_pose[1]
+        self.maze[prev_agent_pose[0]][prev_agent_pose[1]] = 0
+        self.local_map[prev_agent_pose[0]][prev_agent_pose[1]] = 0
+        self.local_map[prev_agent_pose[0]][prev_agent_pose[1]] = self.prev_point_val
+        self.prev_point_val = self.local_map[self.curr_agent_pose[0]][self.curr_agent_pose[1]] - 1.0
+            
+        self.dy = abs(self.target_pose[0] - self.curr_agent_pose[0])
+        self.dx = abs(self.target_pose[1] - self.curr_agent_pose[1])
 
-        self.maze[self.prev_agent_pose[0]][self.prev_agent_pose[1]] = 0
-        
         if self.maze[self.curr_agent_pose[0]][self.curr_agent_pose[1]] == 0:
 
-            if (self.dy < self.shortest_dy) and (self.dx < self.shortest_dx):
+            if self.local_map[self.curr_agent_pose[0]][self.curr_agent_pose[1]] == 0.5:
 
-                self.reward += (math.sqrt((self.height - self.dy)**2 + (self.width - self.dx)**2) + self.visited[self.curr_agent_pose[0]][self.curr_agent_pose[1]] * math.sqrt(self.dy**2 + self.dx**2) * 0.3)
-
-                self.shortest_dy = copy.deepcopy(self.dy)
-                self.shortest_dx = copy.deepcopy(self.dx)
-
-            elif self.visited[self.curr_agent_pose[0]][self.curr_agent_pose[1]] == 0:
-                
                 self.reward += math.sqrt((self.height - self.dy)**2 + (self.width - self.dx)**2)
 
-            else:
+            elif self.local_map[self.curr_agent_pose[0]][self.curr_agent_pose[1]] < 0.5:
 
-                self.reward += (1 + self.visited[self.curr_agent_pose[0]][self.curr_agent_pose[1]] * 0.3)
+                self.reward += self.local_map[self.curr_agent_pose[0]][self.curr_agent_pose[1]]
 
-        self.maze[self.curr_agent_pose[0]][self.curr_agent_pose[1]] = -1
+        self.maze[self.curr_agent_pose[0]][self.curr_agent_pose[1]] = 1
+        self.local_map[self.curr_agent_pose[0]][self.curr_agent_pose[1]] = 1
+        # self.local_map[self.curr_agent_pose[0]][self.curr_agent_pose[1]] = 0
 
-        # self.visited[self.curr_agent_pose[0]][self.curr_agent_pose[1]] += -1
-
-        self.maze_drawer.update_maze(self.visited, curr_pose=self.curr_agent_pose)
+        self.maze_drawer.update_maze(self.local_map, curr_pose=self.curr_agent_pose)
 
     def step(self, action):
 
-        self.reward = 0
+        self.reward = 0.0
         self.done = 0
         success = False
 
         if action == 0:
             self.maze_update(dheight = 0, dwidth = 1)
-            self.reward += -0.1
+            self.reward += -0.01
 
-        if action == 1:
+        elif action == 1:
             self.maze_update(dheight = 0, dwidth = -1)
-            self.reward += -0.1
+            self.reward += -0.01
 
-        if action == 2:
+        elif action == 2:
             self.maze_update(dheight = 1, dwidth = 0)
-            self.reward += -0.1
+            self.reward += -0.01
 
-        if action == 3:
+        elif action == 3:
             self.maze_update(dheight = 1, dwidth = 1)
-            self.reward += -0.1
+            self.reward += -0.01
 
-        if action == 4:
+        elif action == 4:
             self.maze_update(dheight = 1, dwidth = -1)
-            self.reward += -0.1
+            self.reward += -0.01
 
-        if action == 5:
+        elif action == 5:
             self.maze_update(dheight = -1, dwidth = 0)
-            self.reward += -0.1
+            self.reward += -0.01
 
-        if action == 6:
+        elif action == 6:
             self.maze_update(dheight = -1, dwidth = 1)
-            self.reward += -0.1
+            self.reward += -0.01
 
-        if action == 7:
+        elif action == 7:
             self.maze_update(dheight = -1, dwidth = -1)
-            self.reward += -0.1
+            self.reward += -0.01
 
-        if self.done != 1:
-            # Primary Success Reward
-            if (self.dy <= 1) and (self.dx <= 1):
-                self.done = 1
-                self.reward += 100000
-                success = True
-
-            # Secondary Reward
-            elif (self.dy <= 2) and (self.dx <= 2):
-                # self.done = 1
-                self.reward += 1000
-
-        states = [self.dy, self.dx, self.curr_agent_pose[0], self.curr_agent_pose[1]]
+        # Primary Success Reward
+        if ((self.target_pose[0] - self.curr_agent_pose[0]) <= 1) and ((self.target_pose[1] - self.curr_agent_pose[1]) <= 1):
+            self.done = 1
+            self.reward += 1000
+            success = True
+            self.maze_drawer.update_maze(self.local_map, curr_pose=self.curr_agent_pose)
 
         for i in range(-1 * (self.detection_range), self.detection_range + 1):
             for j in range(-1 * (self.detection_range), self.detection_range + 1):
-                if (i == 0) and (j == 0):
-                    self.visited[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] += -1
-                elif (0 <= self.curr_agent_pose[0] + i) and (self.curr_agent_pose[0] + i < self.height) and (0 <= self.curr_agent_pose[1] + j) and (self.curr_agent_pose[1] + j < self.width):
-                    if self.maze[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] == 1:
-                        self.visited[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] = 1
-                    else:
-                        self.visited[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] += -1
 
-        states = np.concatenate((states, self.visited.flatten()))
+                if (i == 0) and (j == 0): 
+                
+                    self.local_map[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] = 1
 
-        return self.reward, states, self.done, success
+                elif self.within_bounds(self.curr_agent_pose[0] + i, 0, self.maze.shape[0]) and self.within_bounds(self.curr_agent_pose[1] + j, 0, self.maze.shape[1]):
+                
+                    if self.maze[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] == 3:
+                
+                        if self.local_map[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] < 3:
+
+                            self.local_map[self.curr_agent_pose[0] + i][self.curr_agent_pose[1] + j] = 3
+
+        states = self.local_map.flatten()
+
+        # reward_range = np.array(list(range(-30, 10000)))
+        # est_reward_mean = reward_range.mean()
+        # est_reward_std = reward_range.std()
+        # est_reward_max = reward_range.max()
+        # est_reward_min =reward_range.min()
+
+        # # self.reward = (self.reward - est_reward_mean) / est_reward_std
+        # self.reward = (self.reward - est_reward_min) / (est_reward_max - est_reward_min)
+        # # print('Min-Max Reward : {}'.format(self.reward))
+
+        # print(states)
+        return states, self.reward, self.done, success
